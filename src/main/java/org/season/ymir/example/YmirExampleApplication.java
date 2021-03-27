94bcb167c315a0753c271a0b4c7db16cd0061158
KevinClair <704714211@qq.com>
2021-03-27 23:48:21 +0800

    project init.

---
+++ src/main/java/org/season/ymir/example/YmirExampleApplication.java
@@ -0,0 +1,13 @@
+package org.season.ymir.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YmirExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YmirExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/season/ymir/example/YmirExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.season.ymir.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YmirExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
